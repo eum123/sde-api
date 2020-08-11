289fd5e8ee1ae2ee9c7f6ede147e97d9f991a833
DEShin <luxisfun@wemakeprice.com>
2020-08-11 16:20:17 +0900

    commit check

--- src/main/resources/query/schema.sql
+++
@@ -1,3 +0,0 @@
-drop table category if exists;
-
-create table category (category_no integer not null, category_name varchar(255), parent_no integer, depth integer not null, primary key (category_no));
